de8b56a18b92afbd71e0a4feb5e085f979ed8071
judith <judith.boshoven@student.hu.nl>
2018-09-20 12:00:57 +0200

    1. getallen, strings and conversion

---
+++ Programming/Les 4/1. Getallen, strings and conversion.py
@@ -0,0 +1,17 @@
+cijferIOR =  7
+cijferPROG = 6
+cijferCSN =  8
+
+gemiddelde = ((cijferIOR+cijferPROG+cijferCSN)/3)
+print(gemiddelde) #7
+beloning = (gemiddelde*30)
+print(beloning)
+
+print('Mijn cijfers (gemiddelde van ' + str(gemiddelde) + ') leveren een beloning van $' + str(beloning) + ' op!')
+
+'''
+output
+7.0
+210.0
+Mijn cijfers (gemiddelde van 7.0) leveren een beloning van $210.0 op!
+'''
